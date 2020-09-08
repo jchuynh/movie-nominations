a3f247c4e518fac23b55fed4fc4bcc52225bae45
Jessica Huynh <chauhuynh.jessica@gmail.com>
2020-09-08 02:00:16 -0700

    Added Psuedocode and problems/solutions to app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
-console.log('app.js');
+// console.log('app.js is working');
 
-// const API_KEY = process.env.REACT_APP_OMBd_API_KEY;
+const API_KEY = process.env.REACT_APP_OMBd_API_KEY;
 
 // import axios from 'axios';
 
 class MovieNominations extends React.Component {
 
 	render() {
-		console.log('movienom');
 		return (
 			<div>
 				<Header />
-				<MovieDisplay />
 				<MovieSearch />
+				<MovieDisplay />
 				<Nomination />
 			</div>
 		);
@@ -22,43 +21,11 @@ class MovieNominations extends React.Component {
 class Header extends React.Component {
 	render() {
 		return (
-			<div>
+			<div className="container">
 				<h1>Movie Nominations</h1>
 				<p>The best of the best!</p>
 			</div>
 		);
-	}
-}
-
-class MovieDisplay extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			movieData: {}
-		};
-	}
-
-	ComponentDidMount() {
-		axios
-			// Allows the user to search for movies on multiple categories.
-			.get(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${this.props.movieID}`)
-			.then(res => res.data)
-            .then(res => {
-                this.setState({ movieData: res });
-            });
-	}
-
-	render() {
-		console.log('display');
-		const {Title, Released} = this.state.movieData;
-
-		return ( 
-			<div>
-				<h2>{Title}</h2>
-				<p>{Released}</p>
-			</div>
-		);	
 	}
 }
 
@@ -68,31 +35,39 @@ class MovieSearch extends React.Component {
 		this.state = {
 			movieList: [],
 			searchTerm: ''
-		}
+		};
+		// Added below so object can bind -- got undefined error
 		this.handleSearch = this.handleSearch.bind(this)
 		this.handleUpdateInput = this.handleUpdateInput.bind(this)
 	}
 
 	handleSearch(e) {
 		e.preventDefault();
-		console.log('handleSearch : ', this.state.searchTerm);
-		axios
-			.get(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${this.state.searchTerm}`)
+		// console.log('handleSearch : ', this.state.searchTerm);
+		// Using Axios to help make the api request
+		axios ({
+			method: 'get',
+			url: `https://www.omdbapi.com/?apikey=${API_KEY}&s=
+				${this.state.searchTerm}`,
+			responseType: 'json'
+		})
+			// looking at json file under a standard Search
+			// If the search exsits, parse the imdbID to movieList arr
+			// if the search is empty, re turn an empty movie list
 			.then(res => res.data)
-			.then(res => {
-				if (!res.handleSearch) {
-					this.setState({ movieList: [] });
-					return;
+			.then(res => {if 
+				(!res.Search) {
+				this.setState({ movieList: [] });
+				return;
 				}
-
-				const movieList = res.handleSearch.map(movie.imdbID);
-				this.setState({movieList});
+				//map the imdbID and save it 
+				const movieList = res.Search.map(movie => movie.imdbID);
+					this.setState({ movieList });
 		});
-		
 	};
 
 	handleUpdateInput(e) {
-		e.preventDefault();
+		// e.preventDefault();
 		console.log('handleUpdateInput : ', this.state.searchTerm);
 		this.setState({
 			searchTerm: e.target.value
@@ -101,23 +76,28 @@ class MovieSearch extends React.Component {
 
 	
 	render() {
-		const { movieList } =  this.state;
-		console.log('search-box');
+		// console.log('handleSearch : ', this.state.searchTerm);
+		const { movieList } = this.state;
+		// console.log('movieList:', movieList);
+		// if the length of the items in the movieList arr is > 0 
+		// map the arr of movie to movieID
+		// movieID will  be sent to <MovieDisplay /> into the API call
 		return (
 			<div>
 				<form onSubmit={this.handleSearch}>
-					<input type='text' placeholder='Type Here' name='searchTerm'
-					onChange = {this.handleUpdateInput}/>
-					<button>Search</button>
+					<input type='text' placeholder='Type Here' 
+						name='searchTerm'
+						onChange={this.handleUpdateInput}/>
+					<button type='submit'>Search</button>
 				</form>
 				{movieList.length > 0 ? (
                     movieList.map(movie => (
-                        <MovieCard movieID={movie} key={movie} />
+                        <MovieDisplay movieID={movie} key={movie} />
                     ))
                 ) : (
                     <p>
-                        Couldn't find any movie. Please search again using
-                        another search criteria.
+						Wasn't able to find a movie. Please try another 
+						search.
 					</p>
 				)}
 			</div>
@@ -125,14 +105,100 @@ class MovieSearch extends React.Component {
 	}
 }
 
-class Nomination extends React.Component {
+
+class MovieDisplay extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			movieData: {},
+			nominationList: []
+		};
+		this.handleAddNomination = this.handleAddNomination.bind(this)
+	}
+
+	ComponentDidMount() {
+		axios({
+			method: 'get',
+			// Search by imdbID to extract all info in json format
+			url: `https://www.omdbapi.com/?apikey=${API_KEY}&i=
+				${this.props.movieID}`,
+			responseType: 'json'
+		})	// something wrong here -- movieData showing {} empty
+			// movieID and movieList checks out
+			// movieID not connecting to get method?
+			// Are keys an issue?
+			.then(data => data.json())
+            .then(data => {
+				this.setState({ movieData: data });
+			});
+	}
+
+	// When the user selects a movie to nominate,
+	// User clicks button
+	// movie gets added to Nomination list
+
+	//MovidID showing up as undefined, cannot push undefined
+	//NominationList is therefore empty
+	handleAddNomination(movieID) {
+		this.setState(() => {
+			const nominationList = this.state.list.push(this.state.movieID)
+			
+		});
+	}
 
 	render() {
-		console.log('nom');
+		console.log('movieData', this.state.movieData);
+		console.log('movieID:', this.props.movieID);
+		console.log('nominationList', this.state.nominationList)
+		// Get only the Title and Released info from the most recent search
+		const {Title, Year} = this.state.movieData;
+		return ( 
+			<div className="movieCard">
+				<h2>{Title}</h2>
+				<p>{Year}</p>
+				<button onClick={this.handleAddNomination}>
+					Add Nomination</button>
+			</div>
+		);	
+	}
+}
+
+// Section where the movies that have been nominated live
+class Nomination extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			movieData: {},
+			nominationList: []
+		};
+		this.handleRemoveNomination = this.handleRemoveNomination.bind(this)
+	}
+
+	// When user selects the Remove Nomination button
+	// Make a copy of the array? No we are not saving the previous set
+	// remove the array by index
+	handleRemoveNomination(e) {
+		e.preventDefault();
+		this.setState(() => {
+			const nominationList = this.state.nominationList
+			.filter(movie => movie.id !== e)
+			this.setState( {nominationList} )
+		});
+	}			
+				
+			
+	render() {
+		const {Title, Year} = this.state.movieData;
 		return (
-			<div>
-				<p>This where the user nominations will go.</p>
-				<button>Remove Nomination</button>
+			<div className="movieCard">
+				<h2>{Title}</h2>
+				<p>{Year}</p>
+				<h1>Here Are Your Nominations!</h1>
+					<div className="movieCard">
+						<button onClick={this.props.handleRemoveNomination}>
+							Remove Nomination</button>
+					</div>
 			</div>
 		);
 	}
